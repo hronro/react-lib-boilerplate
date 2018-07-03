b6e9a86cfd044e946ffda4e24407cb22654b07a3
foisonocean <foisonocean@gmail.com>
2018-07-03 19:57:48 +0800

    :sparkles: auto generate global namespace (for browser build)

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,9 @@ import replace from 'rollup-plugin-replace';
 import resolve from 'rollup-plugin-node-resolve';
 import typescript from 'rollup-plugin-typescript2';
 import closure from 'rollup-plugin-closure-compiler-js';
+
+import camelCase from 'camelcase';
+
 import packageInfo from './package.json';
 
 const isProd = process.env.NODE_ENV === 'production';
@@ -64,7 +67,7 @@ const esConfig = Object.assign({}, baseConfig, {
 const browserConfig = Object.assign({}, baseConfig, {
   external: browserExternalDependencies,
   output: {
-    name: 'MyComponent',
+    name: camelCase(packageInfo.name, {pascalCase: true}),
     file: packageInfo.unpkg,
     format: 'iife',
     globals: baseExternalDependencies,
