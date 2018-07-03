79bd921122703e239205d38d27bccff6366897ba
foisonocean <foisonocean@gmail.com>
2018-07-03 19:51:32 +0800

    :sparkles: replace `process.env.NODE_ENV` in browser build

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,4 @@
+import replace from 'rollup-plugin-replace';
 import resolve from 'rollup-plugin-node-resolve';
 import typescript from 'rollup-plugin-typescript2';
 import closure from 'rollup-plugin-closure-compiler-js';
@@ -70,6 +71,9 @@ const browserConfig = Object.assign({}, baseConfig, {
   },
   plugins: [
     ...baseConfig.plugins,
+    replace({
+      'process.env.NODE_ENV': JSON.stringify('production'),
+    }),
     closure({
       compilationLevel: 'SIMPLE',
       languageIn: 'ECMASCRIPT5_STRICT',
